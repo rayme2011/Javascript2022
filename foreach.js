977f4ff195e3763ace9ad3237f2503afcc16c850
Luis Rayme <rayme2011@gmail.com>
2022-10-27 18:05:39 -0500

    foreach y mas

---
+++ foreach.js
@@ -0,0 +1,18 @@
+var miarras=["luis",5,7,6,9,15,20]	
+
+
+// miarras.forEach( ele => console.log(ele)
+
+// )
+
+
+
+miarras.forEach(( ele,i) => {
+    // console.log(i)
+    
+   if(miarras[i]==15){
+    console.log(`este es el dto ${ele} `)
+   }else{
+   console.log(ele)}
+})
+
