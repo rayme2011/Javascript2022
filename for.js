2ed11198d1242a4dfc951d3537c8192491e7a659
Luis Rayme <rayme2011@gmail.com>
2022-10-27 17:51:17 -0500

    foreach

--- for.js
+++ for.js
@@ -1,5 +1,11 @@
-for (let index = 1; index < 10; index++) {
-        console.log(`ejemplos de for ${index} en datos`);    
+var arrays=["luis",1,10,30,50]
+
+
+
+
+
+for (let index = 0; index < arrays.length; index++) {
+        console.log(`ejemplos for ${arrays[index]} en datos`);    
 }
 
 
